6965d71b7794a9f17b2c7a56f2e5a47f93338b7d
Jack Abdo <jfabdo@gmail.com>
2016-09-24 17:14:02 -0400

    Modularized API

---
+++ R/twitterRESTapi.R
@@ -0,0 +1,42 @@
+#Gets the state ids from twitter
+source("~/R/RTwitterAPI/R/twitter_api_call.R");
+#library(RTwitterAPI)
+load("~/R/getcreds.R")
+
+as.numeric.factor <- function(x) {as.numeric(levels(x))[x]}
+
+fetchfromtwitter <- function(url,query=c()){
+  count <- 15
+  retvalue <- twitter_api_call(url,query,params)
+  print(retvalue)
+  while (retvalue == '{\"errors\":[{\"message\":\"Rate limit exceeded\",\"code\":88}]}')
+  {
+    print("Rate limit reached, waiting")
+    print(count)
+    print("minutes")
+    Sys.sleep(60)
+    retvalue <- twitter_api_call(url,query,params)
+  }
+  return(retvalue)
+}
+
+get_state_geocodes <- function(){
+  statecoords <- read.csv2("~/R/statecoords",sep="\t", header=TRUE);
+  
+  url <- "https://api.twitter.com/1.1/geo/reverse_geocode.json";
+  
+  stategeo <- c()
+  
+  for (i in 1:51) {
+    state <- statecoords[i:i,c('State')]
+    latitude <- statecoords[i:i,c('Latitude')]
+    longitude <- statecoords[i:i,c('Longitude')]
+    query <- c(lat=as.numeric.factor(latitude),long=as.numeric.factor(longitude),granularity="admin")
+    retvalue <- fetchfromtwitter(url,query)
+    tempstate <- c('State'=statecoords$State[i],'geocode'=retvalue)
+    stategeo <- c(tempstate,stategeo)
+  };
+  
+  save(stategeo,file="~/R/stategeocodes.R",ascii=TRUE);
+  
+}
